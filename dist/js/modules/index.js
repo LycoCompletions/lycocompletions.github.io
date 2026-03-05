51d99bc2d96e5cb693ba222bc3585a3a640bccf6
LycoCompletions <completions@lycopodium.com>
2026-03-05 09:53:19 +0800

    jobname cleanup

--- dist/js/modules/index.js
+++ dist/js/modules/index.js
@@ -1,6 +1,7 @@
 export * from './utils.js';
 export * from './date.js';
 export * from './parse.js';
+export * from './jobname.js';
 export { createFilters } from './filters.js';
 export { createDashboard } from './dashboard.js';
 export { createFilesUI } from './files.js';
